4d56d27c86794730c42e9ed68a280ff1cda771e3
pcarlitz <pcarlitz@gmail.com>
2017-12-24 12:52:56 -0600

    typo

--- cron_scheduler/scheduleCron.py
+++ cron_scheduler/scheduleCron.py
@@ -3,7 +3,7 @@ from crontab import CronTab
 def main():
 	from crontab import CronTab
 
-	cron=CronTab(user='{}}') #insert your username
+	cron=CronTab(user='{}') #insert your username
 	job=cron.new(command='python /path/to/script/cot.py') #insert path to cot.py script
 
 	job.minute.every(30) #job will run every 30 minutes.
